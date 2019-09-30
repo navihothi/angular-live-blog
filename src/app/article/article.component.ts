006258212ffcd00d74d915182af30a4f52096ba4
Navi Hothi <navi.hothi@mrmiomp2261.rci.rogers.ca>
2019-09-30 11:02:03 -0400

    added directives

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -2,10 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article',
-  templateUrl: './article.component.html',
+  // templateUrl: './article.component.html',
+  template: `
+   <h3>article title</h3>
+   <p *ngIf="author">{{ author }}</p>
+   <p> {{ articleDate | date: 'short' }} </p>
+   <div>BODY</div>
+  `,
   styleUrls: ['./article.component.scss']
 })
 export class ArticleComponent implements OnInit {
+  author = 'Michael B.';
+  articleDate = new Date();
 
   constructor() {
   // dependency injection
